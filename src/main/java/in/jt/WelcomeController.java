909934d878e32610aa3aa3ea3773e822819ddc68
thanujakaviti <thanujakaviti@gmail.com>
2022-02-11 06:29:29 +0530

    initial commit

---
+++ src/main/java/in/jt/WelcomeController.java
@@ -0,0 +1,16 @@
+package in.jt;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class WelcomeController {
+
+	@GetMapping("/welcome")
+	public String displayWelcomeMsg() {
+		System.out.println("*****************");
+		return "welcome to java techie";
+	}
+}
